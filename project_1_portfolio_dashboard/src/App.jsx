0156be4e97b82cae5aca4d70560ee96d4e2195d7
mulagadaaari <mailaari.m@gmail.com>
2026-02-14 07:44:45 +0530

    clean boiler plate - basic dashboard heading

---
+++ project_1_portfolio_dashboard/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>My Data Portfolio Dashboard</h1>
+      <p>Welcome to my data portfolio dashboard! Here you can find various visualizations and insights from my data projects.</p>
+      <p>M AARI</p>
+    </div>
+  )
+}
+
+export default App
